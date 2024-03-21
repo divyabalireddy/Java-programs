42afaf45d25d7e269f52b1b5b77cfdba497f6810
divyabalireddy <divyabalireddy368@gmail.com>
2024-03-21 16:52:21 +0530

    javaprograms

---
+++ ifelseprogram.java
@@ -0,0 +1,26 @@
+package javaprograms;
+
+public class ifelseprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=25;
+		int weight=55;
+		if(age>=18)
+		{
+			if(weight>50)
+			{
+				System.out.println("eligible for blood donation camp");
+			}
+			else
+			{
+				System.out.println("not eligible for blood donation camp");
+			}
+		}
+			else
+			{
+				System.out.println("age must be greater than 18");
+			}
+				
+			}
+			}
